fbaebf3ff82be107944fc4bed6d2ddbf129d5df1
Iliya <Iliya@Home_PC>
2020-04-25 10:58:47 +0300

    I hope this is it

---
+++ DSA_CWK_1920/src/SongTrack.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+
+public class SongTrack 
+{ 
+    public static void main(String args[]) 
+    { 
+     
+        LinkedList<String> object = new LinkedList<String>(); 
+  
+        
+        object.add("Track ID"); 
+        object.add("Track Title"); 
+        object.addLast("Artist"); 
+        object.addFirst("Track Lenght"); 
+        object.add(2, "Composer"); 
+        object.add("Relese Date"); 
+        object.add("Album"); 
+        object.add("Genre"); 
+        System.out.println("Linked list : " + object); 
+  
+        
+        object.remove("B"); 
+        object.remove(3); 
+        object.removeFirst(); 
+        object.removeLast(); 
+        System.out.println("Linked list after deletion: " + object); 
+  
+        
+        boolean status = object.contains("E"); 
+  
+        if(status) 
+            System.out.println("List contains the element 'E' "); 
+        else
+            System.out.println("List doesn't contain the element 'E'"); 
+  
+        
+        int size = object.size(); 
+        System.out.println("Size of linked list = " + size); 
+  
+        
+        Object element = object.get(2); 
+        System.out.println("Element returned by get() : " + element); 
+        object.set(2, "Y"); 
+        System.out.println("Linked list after change : " + object); 
+    } 
+} 
